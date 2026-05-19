e7749424cfc13621032656e296191746c1bb51e5
Devon Blandin <123595+dblandin@users.noreply.github.com>
2026-05-19 19:53:36 +0200

    feat: add zoxide as cd replacement

--- home/devon/default.nix
+++ home/devon/default.nix
@@ -64,6 +64,12 @@
 
   programs.starship.enable = true;
 
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = [ "--cmd cd" ];
+  };
+
   programs.zsh = {
     enable = true;
     shellAliases = {
